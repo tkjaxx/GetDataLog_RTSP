e71e02cb57b21301e391b0c0056c5ae9cc04ae17
tkjaxx <86125664+tkjaxx@users.noreply.github.com>
2024-03-26 16:37:28 +0800

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Windows.Forms;
+using Ozeki.Media;
+using Ozeki.Camera;
+
+namespace VideoCameraViewer02
+{
+    public partial class Form1 : Form
+    {
+        private IIPCamera _camera;
+        private DrawingImageProvider _imageProvider = new DrawingImageProvider();
+        private MediaConnector _connector = new MediaConnector();
+        private VideoViewerWF _videoViewerWF1;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Create video viewer UI control
+            _videoViewerWF1 = new VideoViewerWF();
+            _videoViewerWF1.Name = "videoViewerWF1";
+            _videoViewerWF1.Size = panel1.Size;
+            panel1.Controls.Add(_videoViewerWF1);
+
+            // Bind the camera image to the UI control
+            _videoViewerWF1.SetImageProvider(_imageProvider);
+        }
+
+        // Connect camera video channel to image provider and start
+        private void connectBtn_Click(object sender, EventArgs e)
+        {
+            //_camera = IPCameraFactory.GetCamera("rtsp://127.0.0.2:14550/9999/Payload", "root", "pass");
+            _camera = IPCameraFactory.GetCamera("rtsp://192.168.2.119", "root", "pass");
+            _connector.Connect(_camera.VideoChannel, _imageProvider);
+            _camera.Start();
+            _videoViewerWF1.Start();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace VideoCameraViewer02
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+        //live555proxyserver -I 9999 -p 14550 -A 127.0.0.2 -N Payload  rtsp://192.168.2.119
+    }
+}
